0aacfa5b3992cbc4cff11ded6a9ab2278583c0e1
NomsuBen <olaniranbenjamin@gmail.com>
2024-05-04 02:51:48 -0400

    Added delete button

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -1,6 +1,4 @@
-let books = [
-
-];
+let books = [];
 
 function addBook() {
     const bookName = document.getElementById('bookName').value;
@@ -15,22 +13,35 @@ function addBook() {
             pagesNumber: pagesNumber
         };
         books.push(book);
-        showbooks();
+        showBooks();
         clearInputs();
     } else {
         alert('Please fill in all fields correctly.');
     }
-    function showbooks() {
-        const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
-    <P><strong>Book Name: </strong>${book.name}</P>
-      <p><strong>Author Name:</strong> ${book.authorName} </p>
-      <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-      <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>`)
-        document.getElementById('books').innerHTML = booksDiv;}
+}
 
-        function clearInputs() {
-            document.getElementById('bookName').value = '';
-            document.getElementById('authorName').value = '';
-            document.getElementById('bookDescription').value = '';
-            document.getElementById('pagesNumber').value = '';}
+function showBooks() {
+    const booksDiv = books.map((book, index) => `
+        <div>
+            <h1>Book Number: ${index + 1}</h1>
+            <p><strong>Book Name:</strong> ${book.name}</p>
+            <p><strong>Author Name:</strong> ${book.authorName}</p>
+            <p><strong>Book Description:</strong> ${book.bookDescription}</p>
+            <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>
+            <button onclick="deleteBook(${index})">Delete</button>
+        </div>`
+    ).join('');
+    document.getElementById('books').innerHTML = booksDiv;
+}
+
+function clearInputs() {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook(index) {
+    books.splice(index, 1);
+    showBooks();
 }
